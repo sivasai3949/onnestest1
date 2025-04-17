5297337e745312563f10d43af2767fd35b0c960d
Naga Siva Sai <162962494+sivasai3949@users.noreply.github.com>
2025-04-17 14:22:56 +0530

    Update index.jsx

--- arino-react/src/components/Footer/index.jsx
+++ arino-react/src/components/Footer/index.jsx
@@ -78,7 +78,7 @@ export default function Footer({ copyrightText, logoSrc, logoAlt, text }) {
       <Div className="container">
         <Div className="cs-bottom_footer">
           <Div className="cs-bottom_footer_left">
-            <Div className="cs-copyright">Copyright © 2022 Laralink.</Div>
+            <Div className="cs-copyright">Copyright © 2025 Onnes Ruskiv Cyrogenics .</Div>
           </Div>
           <Div className="cs-bottom_footer_right">
             <MenuWidget menuItems={copyrightLinks} variant=" cs-style2" />
